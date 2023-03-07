307071427211c340a9724c7a154e136ee459485a
Knarkzel <knarkzel@gmail.com>
2023-03-07 15:24:48 +0100

    error issue fixed

--- src/error.rs
+++ src/error.rs
@@ -4,31 +4,21 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Error {
     description: String,
-    source: Option<Box<Error>>,
 }
 
 impl Error {
     pub fn new<T: std::fmt::Display>(message: T) -> Self {
         Self {
             description: message.to_string(),
-            source: None,
         }
     }
 }
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn 'static + std::error::Error)> {
-        self.source.as_ref().map(|s| &**s as &(dyn 'static + std::error::Error))
-    }
-
-    fn description(&self) -> &str {
-        &self.description
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.description)
+impl<T: std::fmt::Display> From<T> for Error {
+    fn from(value: T) -> Self {
+        Self {
+            description: value.to_string(),
+        }
     }
 }
 
